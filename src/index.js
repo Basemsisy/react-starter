f1f091afce73911b9854cf4feadcea50d413c006
BasemElsisy <basemsisy@gmail.com>
2021-02-03 20:49:47 +0200

    wip:  implement DDD

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
-import App from "./App";
+import App from "app/App";
 import { Provider } from "react-redux";
-import { configureStore } from "store/config";
+import { configureStore } from "app/store/config";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
