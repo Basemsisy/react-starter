80f176098af7ea08338730cb73d521369714c713
BasemElsisy <basemsisy@gmail.com>
2021-02-08 20:53:31 +0200

    wip: header

---
+++ src/app/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Header.module.scss";
+
+const Header = ({ ...props }) => {
+  return <div className={styles.Header}>header</div>;
+};
+
+export default Header;
