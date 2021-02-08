26cdb9d1742c22b98e9ca297dd329a10342bcf77
BasemElsisy <basemsisy@gmail.com>
2021-02-08 20:52:39 +0200

    wip: page generator

--- plopfile.js
+++ plopfile.js
@@ -28,4 +28,34 @@ module.exports = function (plop) {
       },
     ],
   });
+
+  plop.setGenerator("Page", {
+    description: "create an new page",
+    prompts: [
+      {
+        type: "input",
+        name: "pageName",
+        message: "what is your component name?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path:
+          "src/app/pages/{{pascalCase pageName}}/{{pascalCase pageName}}.jsx",
+        templateFile: "plop-templates/Page/Page.jsx.hbs",
+      },
+      {
+        type: "add",
+        path:
+          "src/app/pages/{{pascalCase pageName}}/{{pascalCase pageName}}.module.scss",
+        templateFile: "plop-templates/Page/Page.styles.hbs",
+      },
+      {
+        type: "add",
+        path: "src/app/pages/{{pascalCase pageName}}/index.js",
+        template: "export { default } from './{{pascalCase pageName}}'",
+      },
+    ],
+  });
 };
