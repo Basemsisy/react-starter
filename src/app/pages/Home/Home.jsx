62c6ff37a8d3df354fbd71d5c09252663a8be3c4
basem <basemsisy@gmail.com>
2021-02-19 18:22:03 +0200

    initial home page

---
+++ src/app/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Home.module.scss";
+
+const Home = ({...props}) => {
+  return <div className={styles.Home }>
+    home
+  </div>;
+};
+
+export default Home;
