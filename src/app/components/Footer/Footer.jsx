50910d31c814d88f2bdd1d68185d52d9b266fa9f
basem <basemsisy@gmail.com>
2021-02-19 18:19:52 +0200

    initial footer component

---
+++ src/app/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const Footer = ({...props}) => {
+  return <div className={styles.Footer }>
+    footer
+  </div>;
+};
+
+export default Footer;
